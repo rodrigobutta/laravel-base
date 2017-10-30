e80c4db4fd9825a661d526c5a0dda38634f034b6
Rodrigo Butta <rbutta@gmail.com>
2017-10-29 22:07:35 -0300

    Estilo form 1

--- app/Modules/Form/views/front/view.blade.php
+++ app/Modules/Form/views/front/view.blade.php
@@ -16,70 +16,131 @@
 @section('content')
 
 
-<div class="form-loader">
-  LOADERRRR
+<div class="cover">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <br>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pull-left">
+                    <h1>{{$item->name}}</h1>
+                </div>
+                <div class="pull-right">
+                    {{-- <button class="btn btn-primary customize_form" style="margin-top: 5px;" data-name="1">Customize with Form Builder</button> --}}
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <hr>
+            </div>
+        </div>
+
+    </div>
+
 </div>
-<div class="form-message"></div>
 
 
-    <div class="hero-unit">
-         <h1>{{$item->name}}</h1>
-        </br>
-        <form method="POST" action="{!!route('form.view',['formslug'=>$item->slug])!!}" class="form-horizontal" id="form">
 
-            @foreach($fields as $field)
+<div class="container">
 
-                <div class="control-group">
-                    <label class="control-label" for="{{$field->id_name}}">{{$field->title}}{!! $field->is_required ? ' <span class="required">*</span>' : '' !!}</label>
-                    <div class="controls">
+{{--
+    <div class="row">
+        <div class="col-md-8">
+
+        </div>
+    </div> --}}
 
 
-                    @if ($field->type == 'text' || $field->type == 'email')
+    <div class="row row-m-t">
+        <div class="col-md-8">
 
-                        <input type="text" name="{{$field->id_name}}" id="{{$field->id_name}}" placeholder="{{$field->placeholder}}">
 
-                    @elseif ($field->type == 'textarea')
 
-                        <textarea name="{{$field->id_name}}" id="{{$field->id_name}}" placeholder="{{$field->placeholder}}" rows="8" class="span5"></textarea>
 
-                    @elseif ($field->type == 'select')
+            <form method="POST" action="{!!route('form.view',['formslug'=>$item->slug])!!}" class="form-horizontal" id="form">
 
-                        <select name="{{$field->id_name}}" id="{{$field->id_name}}" >
-                            <option value="" selected=""></option>
+                @foreach($fields as $field)
 
-                            {{-- al ser userfield, tengo valores de tabla que son id, name, title, en vez del texto comun en choice --}}
-                            @if (isset($field->nature) && $field->nature == 'userfield')
+                    <div class="control-group">
+                        <label class="control-label" for="{{$field->id_name}}">{{$field->title}}{!! $field->is_required ? ' <span class="required">*</span>' : '' !!}</label>
+                        <div class="controls">
 
-                                @foreach($field->choices as $choice)
-                                    <option value="{{ $choice->id }}">{{ $choice->title }}</option>
-                                @endforeach
 
-                            @else
+                        @if ($field->type == 'text' || $field->type == 'email')
 
-                                @foreach($field->choices as $choice)
-                                    <option value="{{ $choice->choice }}">{{ $choice->choice }}</option>
-                                @endforeach
+                            <input type="text" name="{{$field->id_name}}" id="{{$field->id_name}}" placeholder="{{$field->placeholder}}">
 
-                            @endif
+                        @elseif ($field->type == 'textarea')
 
-                        </select>
+                            <textarea name="{{$field->id_name}}" id="{{$field->id_name}}" placeholder="{{$field->placeholder}}" rows="8" class="span5"></textarea>
 
-                    @endif
+                        @elseif ($field->type == 'select')
 
+                            <select name="{{$field->id_name}}" id="{{$field->id_name}}" >
+                                <option value="" selected=""></option>
+
+                                {{-- al ser userfield, tengo valores de tabla que son id, name, title, en vez del texto comun en choice --}}
+                                @if (isset($field->nature) && $field->nature == 'userfield')
+
+                                    @foreach($field->choices as $choice)
+                                        <option value="{{ $choice->id }}">{{ $choice->title }}</option>
+                                    @endforeach
+
+                                @else
+
+                                    @foreach($field->choices as $choice)
+                                        <option value="{{ $choice->choice }}">{{ $choice->choice }}</option>
+                                    @endforeach
+
+                                @endif
+
+                            </select>
+
+                        @endif
+
+                        </div>
                     </div>
+
+                @endforeach
+
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-success">Aceptar</button>
+                    <button type="reset" class="btn">Cancelar</button>
                 </div>
 
-            @endforeach
+            </form>
 
-            <div class="form-actions">
-                <button type="submit" class="btn btn-success">Aceptar</button>
-                <button type="reset" class="btn">Cancelar</button>
-            </div>
 
-        </form>
+
+
+
+        </div>
     </div>
 
 
+</div>
+
+<footer>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                Evento organizado por <strong><a href="http://www.maquiel.com.ar" target="_blank">Maquiel</a></strong>.
+            </div>
+        </div>
+
+    </div>
+
+</footer>
 
 
 
@@ -105,7 +166,7 @@
       </div>
     </div>
 
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+    {{-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> --}}
 
 
 @endsection
